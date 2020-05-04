48a68c984a3d51020d93c4aa528f53de74782a92
gozmeister <samgorick.sg@gmail.com>
2020-05-04 17:28:05 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :reviews #Stefani
+  ##new, #create, ##index, ##show, ##edit, #update, ##delete
+  
+  resources :users #Isaac
+  ##new #create ##index ##show ##edit #update ##delete /-/-/ ##login
+  
+  resources :screenplays, only: [:index, :show] #Auz
+  ##index, ##show
+
+  #root - ##homepage of a user #Sam
 end

4367d68c1ff0636c3318f171a4c978fa0c446b61
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-05-05 12:17:00 -0500

    added create and update functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :reviews, only: [:index, :show]
+  resources :reviews
   #Stefani
   ##new, #create, ##edit, #update, ##delete
   

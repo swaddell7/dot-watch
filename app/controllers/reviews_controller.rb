69d7fcab1086906b9693dbd036df0efeddfab5ca
swaddell7 <stefani.waddell@hotmail.com>
2020-05-04 20:48:36 -0500

    added reviews controller, routes, aand index/show pages

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,11 @@
+class ReviewsController < ApplicationController
+  
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :reviews #Stefani
-  ##new, #create, ##index, ##show, ##edit, #update, ##delete
+  resources :reviews, only: [:index, :show]
+  #Stefani
+  ##new, #create, ##edit, #update, ##delete
   
   resources :users #Isaac
   ##new #create ##index ##show ##edit #update ##delete /-/-/ ##login

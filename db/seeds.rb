9c69e23d815f603e9da10c1a3a143ef1a8dbfe0c
swaddell7 <stefani.waddell@hotmail.com>
2020-05-08 12:02:54 -0500

    seeded screenplaygenres

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Review.destroy_all
 Like.destroy_all
 ScreenplayGenre.all
 
-genres = %w[Action/Adventure Anime Anthology Biography Comedy Drama Fantasy FilmNoir Historical Horror Musical ScienceFiction Thriller]
+genres = %w[Action/Adventure Animation Anthology Biography Comedy Drama Fantasy GameShow Historical Horror Musical Reality RomCom ScienceFiction SitCom Thriller]
 
 genres.each do |genre|
   Genre.create(name: genre)
@@ -30,7 +30,7 @@ Screenplay.create!(category: "Film", picture_url: "https://m.media-amazon.com/im
 Screenplay.create!(category: "Film", picture_url: "https://www.bet.com/celebrities/photos/2012/11/the-cast-of-8-mile-where-are-they-now/_jcr_content/mainCol/imagegallerycontainer/galleryimage_0.custom750x0.dimg/__1532544698580__1532544679827/072518-celebrities-eminem-8-mile-shot.jpg", title: "8 Mile", description: "8 Mile is a 2002 American hip-hop musical film written by Scott Silver, directed by Curtis Hanson, and starring Eminem, Mekhi Phifer, Brittany Murphy, Michael Shannon, Anthony Mackie, and Kim Basinger", runtime: 118)
 Screenplay.create!(category: "TV Show", picture_url: "https://static.next-episode.net/tv-shows-images/huge/burn-notice.jpg", title: "Burn Notice", description: "A spy recently disavowed by the U.S. government uses his special ops training to help others in trouble", num_seasons: 7, num_episodes: 111, runtime: 44)
 Screenplay.create!(category: "Film", picture_url: "https://reporter.rit.edu:8443/sites/pubDir/slideShow/02-20/1521-2464-158882019.png", title: "The Joker", description: "Joker is a 2019 American psychological thriller film directed and produced by Todd Phillips, who co-wrote the screenplay with Scott Silver. The film, based on DC Comics characters, stars Joaquin Phoenix as the Joker and provides a possible origin story for the character.", runtime: 122)
-Screenplay.create!(category: "TV Show", picture_url: "https://image.pbs.org/contentchannels/2969/jG4GHTFNaKemFyVHmZFP3Q.jpg", title: "The Great British Baking Show", description: "A talented batch of amateur bakers face off in a 10-week competition, whipping up their best dishes in the hopes of being named the UK's best.", num_seasons: 7, num_episodes: 70, runtime 57)
+Screenplay.create!(category: "TV Show", picture_url: "https://image.pbs.org/contentchannels/2969/jG4GHTFNaKemFyVHmZFP3Q.jpg", title: "The Great British Baking Show", description: "A talented batch of amateur bakers face off in a 10-week competition, whipping up their best dishes in the hopes of being named the UK's best.", num_seasons: 7, num_episodes: 70, runtime: 57)
 Screenplay.create!(category: "Film", picture_url: "https://lh3.googleusercontent.com/auIs5tjWlLYaFPGClZOJ7m5YVbnX6uBvz0X02r8TkwFKdzE53ww2MqWSS9gU0YNqoYwvpg", title: "The Dark Knight", description: "With the help of allies Lt. Jim Gordon (Gary Oldman) and DA Harvey Dent (Aaron Eckhart), Batman (Christian Bale) has been able to keep a tight lid on crime in Gotham City. But when a vile young criminal calling himself the Joker (Heath Ledger) suddenly throws the town into chaos, the caped Crusader begins to tread a fine line between heroism and vigilantism.", runtime: 152)
 Screenplay.create!(category: "Film", picture_url: "https://i.insider.com/57d6c8e8dd0895cc308b491a?width=1100&format=jpeg&auto=webp", title: "Mulan", description: "Fearful that her ailing father will be drafted into the Chinese military, Mulan (Ming-Na Wen) takes his spot -- though, as a girl living under a patriarchal regime, she is technically unqualified to serve. She cleverly impersonates a man and goes off to train with fellow recruits. Accompanied by her dragon, Mushu (Eddie Murphy), she uses her smarts to help ward off a Hun invasion, falling in love with a dashing captain along the way.", runtime: 88)
 Screenplay.create!(category: "Film", picture_url: "https://3yoyr53fr4c8sig6z3tnt3tj-wpengine.netdna-ssl.com/wp-content/uploads/2018/06/GreatestShowman-1.jpg", title: "The Greatest Showman", description: "Inspired by the imagination of P.T. Barnum, The Greatest Showman is an original musical that celebrates the birth of show business & tells of a visionary who rose from nothing to create a spectacle that became a worldwide sensation.", runtime: 106)
@@ -50,9 +50,11 @@ Screenplay.create!(category: "TV Show", picture_url: "https://www.gstatic.com/tv
 Screenplay.create!(category: "TV Show", picture_url: "https://www.gstatic.com/tv/thumb/tvbanners/17114349/p17114349_b_v8_aa.jpg", title: "Grey's Anatomy", description: "The medical drama series focuses on a group of young doctors at Seattle Grace Mercy West Hospital, who began their careers at the facility as interns. One of the young doctors and the show's namesake, Meredith Grey, is the daughter of a famous surgeon. Grey struggles to maintain relationships with her colleagues, particularly the hospital's one-time chief of surgery, Richard Webber, due to a pre-existing relationship between them - Webber and Meredith's mother had a personal relationship when Meredith was young.", num_seasons: 16, num_episodes: 356, runtime: 48)
 Screenplay.create!(category: "TV Show", picture_url: "https://www.gstatic.com/tv/thumb/tvbanners/16944746/p16944746_b_v8_ac.jpg", title: "Black Mirror", description: "Featuring stand-alone dramas -- sharp, suspenseful, satirical tales that explore techno-paranoia -- 'Black Mirror' is a contemporary reworking of 'The Twilight Zone' with stories that tap into the collective unease about the modern world. Each story features its own cast of unique characters, including stars like Bryce Dallas Howard ('The Help'), Alice Eve, Gugu Mbatha-Raw, Tom Cullen and Jerome Flynn ('Game of Thrones'). Joe Wright, Dan Trachtenberg, and James Watkins are among the featured directors.", num_seasons: 5, num_episodes: 22, runtime: 41)
 Screenplay.create!(category: "TV Show", picture_url: "https://www.gstatic.com/tv/thumb/tvbanners/17212373/p17212373_b_v8_ab.jpg", title: "Jeopardy", description: "'Jeopardy!'' is a classic game show -- with a twist. The answers are given first, and the contestants supply the questions. Three contestants, including the previous show's champion, compete in six categories and in three rounds (with each round's 'answers' being worth more prize money). In the third round, 'Final Jeopardy,'' the contestants can name their own jackpot -- as long as it's within the amount of money they've already earned. If a player finishes the second round with zero dollars, they are eliminated from 'Final Jeopardy.' The first version of 'Jeopardy!,' which aired from 1964 to 1975 on NBC, was hosted by Art Fleming. Alex Trebek is the current host; he began with the program in 1984 (at the start of its syndicated run).", num_seasons: 35, num_episodes: 8000, runtime: 24)
-
-
-
+Screenplay.create!(category: "Film", picture_url: "https://www.gstatic.com/tv/thumb/v22vodart/17458/p17458_v_v8_ac.jpg", title: "Jumanji", description: "A magical board game unleashes a world of adventure on siblings Peter (Bradley Pierce) and Judy Shepherd (Kirsten Dunst). While exploring an old mansion, the youngsters find a curious, jungle-themed game called Jumanji in the attic. When they start playing, they free Alan Parrish (Robin Williams), who's been stuck in the game's inner world for decades. If they win Jumanji, the kids can free Alan for good -- but that means braving giant bugs, ill-mannered monkeys and even stampeding rhinos!", runtime: 104)
+Screenplay.create!(category: "Film", picture_url: "https://www.gstatic.com/tv/thumb/v22vodart/11491197/p11491197_v_v8_ab.jpg", title: "Steve Jobs", description: "With public anticipation running high, Apple Inc. co-founders Steve Jobs (Michael Fassbender) and Steve 'Woz' Wozniak get ready to unveil the first Macintosh in 1984. Jobs must also deal with personal issues related to ex-girlfriend Chrisann Brennan and their young daughter Lisa. Eventually fired, Jobs launches NeXT Inc. and prepares to release a new computer model in 1988. Ten years later, Jobs is back at Apple Inc. and about to revolutionize the industry once again with the iMac.", runtime: 122)
+Screenplay.create!(category: "Film", picture_url: "https://www.gstatic.com/tv/thumb/v22vodart/12465613/p12465613_v_v8_aa.jpg", title: "Sully", description: "On Jan. 15, 2009, Capt. Chesley 'Sully' Sullenberger (Tom Hanks) tries to make an emergency landing in New York's Hudson River after US Airways Flight 1549 strikes a flock of geese. Miraculously, all of the 155 passengers and crew survive the harrowing ordeal, and Sullenberger becomes a national hero in the eyes of the public and the media. Despite the accolades, the famed pilot now faces an investigation that threatens to destroy his career and reputation.", runtime: 96)
+Screenplay.create!(category: "Film", picture_url: "https://www.gstatic.com/tv/thumb/v22vodart/12732076/p12732076_v_v8_aa.jpg", title: "Hush", description: "A deaf writer who retreated into the woods to live a solitary life must fight for her life in silence when a masked killer appears in her window.", runtime: 87)
+Screenplay.create!(category: "Film", picture_url: "https://www.gstatic.com/tv/thumb/v22vodart/10980706/p10980706_v_v8_ab.jpg", title: "The Martian", description: "When astronauts blast off from the planet Mars, they leave behind Mark Watney (Matt Damon), presumed dead after a fierce storm. With only a meager amount of supplies, the stranded visitor must utilize his wits and spirit to find a way to survive on the hostile planet. Meanwhile, back on Earth, members of NASA and a team of international scientists work tirelessly to bring him home, while his crew mates hatch their own plan for a daring rescue mission.", runtime: 151)
 
 20.times do
   Review.create!(user_id: User.all.ids.sample, screenplay_id: Screenplay.all.ids.sample, title: Faker::Quotes::Shakespeare.hamlet_quote, description: Faker::Quotes::Shakespeare.romeo_and_juliet_quote, rating: rand(6))
@@ -62,7 +64,40 @@ end
   Like.create!(review_id: Review.all.ids.sample, user_id: User.all.ids.sample)
 end
 
-Screenplay.all.each do |screen|
-  ScreenplayGenre.create!(screenplay_id: screen.id, genre_id: Genre.all.ids.sample)
-end
+# Screenplay.all.each do |screen|
+#   ScreenplayGenre.create!(screenplay_id: screen.id, genre_id: Genre.all.ids.sample)
+# end
 
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "The Shawshank Redemption").id, genre_id: Genre.find_by(name: "Drama").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Tiger King").id, genre_id: Genre.find_by(name: "Reality").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Sleepless in Seattle").id, genre_id: Genre.find_by(name: "RomCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "You've Got Mail").id, genre_id: Genre.find_by(name: "RomCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Cool Runnings").id, genre_id: Genre.find_by(name: "Comedy").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "8 Mile").id, genre_id: Genre.find_by(name: "Drama").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Burn Notice").id, genre_id: Genre.find_by(name: "Action/Adventure").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "The Joker").id, genre_id: Genre.find_by(name: "Thriller").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "The Great British Baking Show").id, genre_id: Genre.find_by(name: "Reality").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "The Dark Knight").id, genre_id: Genre.find_by(name: "Action/Adventure").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Mulan").id, genre_id: Genre.find_by(name: "Animation").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "The Greatest Showman").id, genre_id: Genre.find_by(name: "Musical").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "About Time").id, genre_id: Genre.find_by(name: "RomCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Dan in Real Life").id, genre_id: Genre.find_by(name: "Drama").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "The Proposal").id, genre_id: Genre.find_by(name: "RomCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Back to the Future").id, genre_id: Genre.find_by(name: "Action/Adventure").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Napoleon Dynamite").id, genre_id: Genre.find_by(name: "Comedy").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Fever Pitch").id, genre_id: Genre.find_by(name: "RomCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Indiana Jones and the Last Crusade").id, genre_id: Genre.find_by(name: "Action/Adventure").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Community").id, genre_id: Genre.find_by(name: "SitCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Bob's Burgers").id, genre_id: Genre.find_by(name: "Animation").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "The Office").id, genre_id: Genre.find_by(name: "SitCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "New Girl").id, genre_id: Genre.find_by(name: "SitCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Schitt's Creek").id, genre_id: Genre.find_by(name: "SitCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Gilmore Girls").id, genre_id: Genre.find_by(name: "SitCom").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Grey's Anatomy").id, genre_id: Genre.find_by(name: "Drama").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Black Mirror").id, genre_id: Genre.find_by(name: "Anthology").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Jeopardy").id, genre_id: Genre.find_by(name: "GameShow").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Jumanji").id, genre_id: Genre.find_by(name: "Fantasy").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Steve Jobs").id, genre_id: Genre.find_by(name: "Biography").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Sully").id, genre_id: Genre.find_by(name: "Historical").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "Hush").id, genre_id: Genre.find_by(name: "Horror").id)
+ScreenplayGenre.new(screenplay_id: Screenplay.find_by(title: "The Martian").id, genre_id: Genre.find_by(name: "ScienceFiction").id)
